734000f141b9bb4d1983ed4eff8edee27a5d0169
Hadrien Boyer <hello@bytebakers.app>
2023-02-08 21:20:58 +0100

    refactor & gitignore w/ .env deleted

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 
 **GETTING STARTED**
 
-OpenAI wrapper in NodeJS for 🐼 __PANDA__ (project in progress, you should expect some updates frequently) 
+https://openai.com: OpenAI(https://okokko) wrapper in NodeJS for 🐼 __PANDA__ (project in progress, you should expect some updates frequently) 
 
-1. Installation: via the terminal, go to the folder of the project and type `$ npm install `
+1. Installation: run the terminal, reach the parent folder of the project and then send this command: `$ npm install `
 2. Run the project: `$ npm run start`
-3. 
+
+Et voilà!
--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-let got = require('got');
-let proc = process;
+//PANDA: index page.
 
-//TODO: hide this secret key!
-proc.env.OPENAI_SECRET_KEY = 'sk-*********';
-console.log(proc.env.OPENAI_SECRET_KEY);
+const _got = require('got');
+require('dotenv').config();
 
-const prompt = `Artist: Kendrick Lamar\n\nLyrics:\n`;
 
-const engines = ({
-    GPT3TextDavinci003: "text-davinci-003",
-    GPT3TextDavinci002: "text-davinci-002",
-    GPT3TextCurie001: "text-curie-001",
-    GPT3TextBabbage001: "text-babbage-001",
-    GPT3TextAda001: "text-ada-001",
-    GPT3TextDavinci001: "text-davinci-001",
-    GPT3DavinciInstructBeta: "davinci-instruct-beta",
-    GPT3Davinci: "davinci",
-    GPT3CurieInstructBeta: "curie-instruct-beta",
-    GPT3Curie: "curie",
-    GPT3Ada: "ada",
-    GPT3Babbage: "babbage"
-});
-const selectedEngine = engines.GPT3Ada;
+//TODO: create a config file for those selections:
+let userConfig = {
+    engines: {
+        GPT3TextDavinci003: "text-davinci-003",
+        GPT3TextDavinci002: "text-davinci-002",
+        GPT3TextCurie001: "text-curie-001",
+        GPT3TextBabbage001: "text-babbage-001",
+        GPT3TextAda001: "text-ada-001",
+        GPT3TextDavinci001: "text-davinci-001",
+        GPT3DavinciInstructBeta: "davinci-instruct-beta",
+        GPT3Davinci: "davinci",
+        GPT3CurieInstructBeta: "curie-instruct-beta",
+        GPT3Curie: "curie",
+        GPT3Ada: "ada",
+        GPT3Babbage: "babbage"
+    },
+    selectedEngine: undefined,
+    //TODO: get and inject the prompt from the DOM (from user button event):
+    prompt: `Hello, may I ask you why the sky is blue?\n`
+};
 
-/*const engines = ({
-    ada:'ada',
-    davinci:'davinci',
-});*/
-
+userConfig.selectedEngine = userConfig.engines.GPT3Ada;
 
 (async () => {
-    const url = 'https://api.openai.com/v1/engines/' + selectedEngine + '/completions';
+    const url = `https://api.openai.com/v1/engines/${userConfig.selectedEngine}/completions`;
     const params = {
-        "prompt": prompt,
+        "prompt": userConfig.prompt,
         "max_tokens": 160,
         "temperature": 0.7,
         "frequency_penalty": 0.5
     };
     const headers = {
-        'Authorization': `Bearer ${proc.env.OPENAI_SECRET_KEY}`,
+        'Authorization': `Bearer ${process.env.OPENAI_SECRET_KEY}`,
     };
 
     let output;
     try {
-        const response = await got.post(url, {json: params, headers: headers}).json();
-        output = `${prompt}${response.choices[0].text}`;
+        const response = await _got.post(url, { json: params, headers: headers }).json();
+        output =
+            `=================================
+             \n=> USER PROMPT: ${userConfig.prompt}\n=================================
+             \n=> RESPONSE FROM THE AI: ${response.choices[0].text}\n=================================\n
+        `;
+
         console.log(output);
+
     } catch (err) {
         console.log(err);
     }
